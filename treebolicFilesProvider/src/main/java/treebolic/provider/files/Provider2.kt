d4a33de967bb82131c7c5be9ca6529718a36c6ec
1313ou <1313ou@gmail.com>
2024-08-02 11:02:41 +0200

    to kotlin

--- treebolicFilesProvider/src/main/java/treebolic/provider/files/Provider2.kt
+++ treebolicFilesProvider/src/main/java/treebolic/provider/files/Provider2.kt
@@ -1,86 +1,67 @@
 /*
  * Copyright (c) 2023. Bernard Bou
  */
+package treebolic.provider.files
 
-package treebolic.provider.files;
+import treebolic.glue.iface.Image
+import treebolic.model.Model
+import treebolic.model.MutableEdge
+import treebolic.model.MutableNode
+import java.net.URL
+import java.util.Properties
 
-import java.net.URL;
-import java.util.Properties;
+class Provider2 : Provider() {
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import treebolic.glue.iface.Image;
-import treebolic.model.Model;
-import treebolic.model.MutableEdge;
-import treebolic.model.MutableNode;
+    // P A R S E
+    /**
+     * Make model
+     *
+     * @param source     source
+     * @param base       base
+     * @param parameters parameters
+     * @return model if successful
+     */
+    override fun makeModel(source: String, base: URL, parameters: Properties): Model? {
+        val model = super.makeModel(source, base, parameters)
+        return if (model == null) null else Model(model.tree, model.settings, images2)
+    }
 
-public class Provider2 extends treebolic.provider.files.Provider
-{
-	// D E C O R A T I O N   M E M B E R S
+    // D E C O R A T E
 
-	static final Image[] images2 = makeImages(images);
+    override fun setNodeImage(node: MutableNode, index: Int) {
+        if (index != -1) {
+            node.imageIndex = index
+        }
+    }
 
-	// P A R S E
+    override fun setTreeEdgeImage(node: MutableNode, index: Int) {
+        if (index != -1) {
+            node.edgeImageIndex = index
+        }
+    }
 
-	/**
-	 * Make model
-	 *
-	 * @param source     source
-	 * @param base       base
-	 * @param parameters parameters
-	 * @return model if successful
-	 */
-	@Nullable
-	@Override
-	public Model makeModel(final String source, final URL base, final Properties parameters)
-	{
-		Model model = super.makeModel(source, base, parameters);
-		return model == null ? null : new Model(model.tree, model.settings, images2);
-	}
+    override fun setEdgeImage(edge: MutableEdge, index: Int) {
+        if (index != -1) {
+            edge.imageIndex = index
+        }
+    }
 
-	// D E C O R A T E
+    companion object {
 
-	@Override
-	public void setNodeImage(@NonNull final MutableNode node, final int index)
-	{
-		if (index != -1)
-		{
-			node.setImageIndex(index);
-		}
-	}
+        val images2: Array<Image?> = makeImages(images)
 
-	@Override
-	public void setTreeEdgeImage(@NonNull final MutableNode node, final int index)
-	{
-		if (index != -1)
-		{
-			node.setEdgeImageIndex(index);
-		}
-	}
-
-	@Override
-	public void setEdgeImage(@NonNull final MutableEdge edge, final int index)
-	{
-		if (index != -1)
-		{
-			edge.setImageIndex(index);
-		}
-	}
-
-	/**
-	 * ImageFactory
-	 *
-	 * @param imageUrls image urls
-	 * @return images
-	 */
-	@NonNull
-	public static Image[] makeImages(@NonNull final String[] imageUrls)
-	{
-		Image[] images = new Image[imageUrls.length];
-		for (int i = 0; i < imageUrls.length; i++)
-		{
-			images[i] = new treebolic.glue.Image(Provider2.class.getResource("images/" + imageUrls[i]));
-		}
-		return images;
-	}
+        /**
+         * ImageFactory
+         *
+         * @param imageUrls image urls
+         * @return images
+         */
+        private fun makeImages(imageUrls: Array<String>): Array<Image?> {
+            val images = arrayOfNulls<Image>(imageUrls.size)
+            for (i in imageUrls.indices) {
+                images[i] = treebolic.glue.Image(Provider2::class.java.getResource("images/" + imageUrls[i]))
+            }
+            return images
+        }
+    }
 }
