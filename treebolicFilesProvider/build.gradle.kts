1333fa3d537425d9fbbc4a05a91b1e633fea0e57
1313ou <1313ou@gmail.com>
2025-06-15 18:57:08 +0200

    to kts

---
+++ treebolicFilesProvider/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+private val vCompileSdk by lazy { rootProject.extra["compileSdk"] as Int }
+private val vMinSdk by lazy { rootProject.extra["minSdk"] as Int }
+
+android {
+
+    namespace = "treebolic.provider.files"
+
+    compileSdk = vCompileSdk
+
+    defaultConfig {
+        minSdk = vMinSdk
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility =JavaVersion.VERSION_17
+        targetCompatibility =JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.treebolic.model)
+    implementation(libs.treebolic.mutable)
+    implementation(libs.treebolic.provider.files)
+
+    implementation(project(":treebolicGlue"))
+
+    implementation(libs.annotation)
+
+    implementation(libs.core.ktx)
+    coreLibraryDesugaring(libs.desugar)
+}
